e9ff6fb07cbef3b7843be02bd96ab85a160429aa
github_Jaswanth560 <email.jaswanth@9908@gmail.com>
2021-04-22 15:51:18 +0530

    Dynamic data

--- dynamicData/script.js
+++ dynamicData/script.js
@@ -1,20 +1,20 @@
 function create(){
-    var input=document.getElementById("input").value;
-    var para2=document.getElementById("para2").value;
-    var output=document.getElementById("output").value;
-    var para3=document.getElementById("para3").value;
+    var employeeName=document.getElementById("employeeName").value;
+    var employeeId=document.getElementById("employeeId").value;
+    var employeeDesignation=document.getElementById("employeeDesignation").value;
+    var employeeLocation=document.getElementById("employeeLocation").value;
 
-    var table=document.getElementsByTagName('table')[0];
+    var tabledata=document.getElementsByTagName('table')[0];
 
-    var newRow=table.insertRow(1);
+    var newRow=tabledata.insertRow(1);
 
-    var cel1=newRow.insertCell(0);
-    var cel2=newRow.insertCell(1);
-    var cel3=newRow.insertCell(2);
-    var cel4=newRow.insertCell(3);
+    var coloumn1Data=newRow.insertCell(0);
+    var coloumn2Data=newRow.insertCell(1);
+    var coloumn3Data=newRow.insertCell(2);
+    var coloumn4data=newRow.insertCell(3);
 
-    cel1.innerHTML=input;
-    cel2.innerHTML=para2;
-    cel3.innerHTML=output;
-    cel4.innerHTML=para3;
+    coloumn1Data.innerHTML=employeeName;
+    coloumn2Data.innerHTML=employeeId;
+    coloumn3Data.innerHTML=employeeDesignation;
+    coloumn4data.innerHTML=employeeLocation;
 }
