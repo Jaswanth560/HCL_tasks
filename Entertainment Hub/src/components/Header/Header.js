b2a6417383a64d7269ca2779aa9436ecfd0207d0
github_Jaswanth560 <email.jaswanth@9908@gmail.com>
2021-07-30 15:46:02 +0530

    Entertainment hub

---
+++ Entertainment Hub/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <span onClick={() => window.scroll(0, 0)} className="header">
+     My Show 🎬 
+    </span>
+  );
+};
+
+export default Header;
